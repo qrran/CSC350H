53387d1b58405f3e14672acfe3b3ef98bb531d66
qrran <dan.lin001@stu.bmcc.cuny.edu>
2024-02-06 13:28:55 -0500

    Log Message Completed

---
+++ HW/HW1b/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise10
+{
+    class Program
+    {
+        // use switch to make the options active, not just printing out
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("**************");
+            Console.Write(">");
+        }
+    }
+}
---
+++ HW/LogMessage/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LogMessage
+{
+    class Program
+    {
+        // static void Main(string[] args)
+        // {
+
+        // }
+        public class LogMessage
+        {
+            private String machineId;
+            private String description;
+
+            ////////////// Part a /////////////////////////
+            public LogMessage(String message)
+            {
+                String[] messageSeparate = message.Split(":");
+                machineId = messageSeparate[0];
+                description = messageSeparate[1];
+                // message = machineID + ":" + description;
+                // Console.WriteLine(message);
+            }
+
+            ///////////////// Part b ////////////////////////
+            // public Boolean containsWord(String keyword)
+            // {
+            //     String[] descriptionSeparate = description.Split(" ");
+            //     for (int i = 0; i < descriptionSeparate.Length; i++)
+            //     {
+            //         if (keyword == descriptionSeparate[i])
+            //             return true;
+            //     }
+            //     return false;
+            // }
+
+            ///////////////// Part b ////////////////////////
+            public Boolean containsWord(String keyword)
+            {
+                String[] descriptionSeparate = description.Split(" ");
+                foreach (String word in descriptionSeparate)
+                {
+                    if (word == keyword)
+                        return true;
+                }
+                return false;
+            }
+
+            public String getMachineId()
+            {
+                return machineId;
+            }
+            public String getDescription()
+            {
+                return description;
+            }
+        }
+    }
+}
